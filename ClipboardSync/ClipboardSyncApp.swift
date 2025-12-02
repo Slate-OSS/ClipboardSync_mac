dfa67b14ef5673f9c7ab296f1c97f8bf1ffb063b
Atharva <finitecode.contact@gmail.com>
2025-12-02 13:32:34 +0530

    Initial Commit

---
+++ ClipboardSync/ClipboardSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardSyncApp.swift
+//  ClipboardSync
+//
+//  Created by Atharva on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardSyncTests/ClipboardSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardSyncTests.swift
+//  ClipboardSyncTests
+//
+//  Created by Atharva on 02/12/25.
+//
+
+import Testing
+@testable import ClipboardSync
+
+struct ClipboardSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
